1477a6adce820de48e673b469758b80fd27e30d2
Ahmed Nasr Eldien <Ahmed.Nasr@linkdev.com>
2024-03-18 21:09:46 +0200

    DAL for Department Module

---
+++ Route.C41.G02.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Route.C41.G02.DAL.Data.Configurations;
+using Route.C41.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Route.C41.G02.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .; Database = MVCApplicationG02; Trusted_Connection = True;");
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+
+        public DbSet<Department> Departments { get; set; }
+    }
+}
